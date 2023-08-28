ac3c9999616768923c5966eea0e0f9b139cf23dc
vikram choudhary <vikram.work.code@gmail.com>
2023-08-28 10:06:14 +0530

    Added a delete function

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const input_field = document.querySelector("#input_field");
-const submit_btn = document.querySelector("#submit_btn");
+const form = document.querySelector("form");
 const delete_btn = document.querySelector("#delete_btn");
 const task_list = document.querySelector("#task_list");
 
-submit_btn.addEventListener("click", (e) => {
-  e.preventDefault;
-  console.log(input_field.value);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
   let li = document.createElement("li");
   let task = document.createTextNode(input_field.value);
   li.appendChild(task);
   task_list.appendChild(li);
+  localStorage;
   input_field.value = "";
+
+  // Delete a li item on click
+  li.onclick = function () {
+    this.parentNode.removeChild(this);
+  };
 });
